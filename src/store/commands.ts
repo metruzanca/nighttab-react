e00baba203e675459ce41b77f02a86aaccb27dc7
Samuele Zanca <samuele.zanca@dechit.it>
2021-06-17 15:32:59 -0400

    All group commands implemented

--- src/store/commands.ts
+++ src/store/commands.ts
@@ -1,5 +1,6 @@
 import { actions } from "./ducks"
 import { Dispatch } from "redux"
+import { BookmarkGroup } from "types"
 
 declare global {
   interface Window {
@@ -11,8 +12,29 @@ declare global {
 
 export default function registerCommands(dispatch: Dispatch) {
   window.App = {
-    createBookmark() { dispatch(actions.bookmarks.createBookmark()) },
-    setEditing(isEditing: boolean) { dispatch(actions.edit.setEditing(isEditing)) },
-    toggleEditing() { dispatch(actions.edit.toggle()) },
+    // Edit mode
+    setEditing(isEditing: boolean) {
+      dispatch(actions.edit.setEditing(isEditing))
+    },
+    toggleEditing() {
+      dispatch(actions.edit.toggle())
+    },
+    // Bookmark Groups
+    createGroup(group: BookmarkGroup) { 
+      dispatch(actions.bookmarks.createGroup(group))
+    },
+    deleteGroup(index: number) {
+      dispatch(actions.bookmarks.deleteGroup(index))
+    },
+    editGroup(group: Partial<BookmarkGroup>, index: number) {
+      dispatch(actions.bookmarks.editGroup(group, index))
+    },
+    moveGroup(from: number, to: number) {
+      dispatch(actions.bookmarks.moveGroup(from, to))
+    },
+    // Bookmarks
+    createBookmark() {
+      dispatch(actions.bookmarks.createBookmark())
+    },
   }
 }
--- src/store/ducks/bookmarks.ts
+++ src/store/ducks/bookmarks.ts
@@ -6,11 +6,23 @@ type INIT =  `${scope}/INIT`
 type init = Action<INIT>
 
 type CREATE_GROUP = `${scope}/CREATE_GROUP`
-type createGroup = Action<CREATE_GROUP>
+type createGroup = PayloadAction<CREATE_GROUP, {
+  group: BookmarkGroup,
+  index: number,
+}>
 type EDIT_GROUP = `${scope}/EDIT_GROUP`
-type editGroup = Action<EDIT_GROUP>
+type editGroup = PayloadAction<EDIT_GROUP, {
+  group: Partial<BookmarkGroup>,
+  index: number,
+}>
 type DELETE_GROUP = `${scope}/DELETE_GROUP`
-type deleteGroup = Action<DELETE_GROUP>
+type deleteGroup = PayloadAction<DELETE_GROUP, number>
+type MOVE_GROUP = `${scope}/MOVE_GROUP`
+type moveGroup = PayloadAction<MOVE_GROUP, {
+  from: number,
+  to: number,
+}>
+
 
 type CREATE_BOOKMARK = `${scope}/CREATE_BOOKMARK`
 type createBookmark = PayloadAction<CREATE_BOOKMARK, {name: string}>
@@ -19,14 +31,14 @@ type editBookmark = Action<EDIT_BOOKMARK>
 type DELETE_BOOKMARK = `${scope}/DELETE_BOOKMARK`
 type deleteBookmark = Action<DELETE_BOOKMARK>
 
-// TODO handle change bookmark order or move to another group
-// TODO handle group order change
+// TODO handle moving bookmark between groups
 
 type ActionTypes =
 | init
 | createGroup
 | editGroup
 | deleteGroup
+| moveGroup
 | createBookmark
 | editBookmark
 | deleteBookmark
@@ -41,17 +53,108 @@ export const bookmarksReducer: Reducer<bookmarksState, ActionTypes> = (
   state = defaultState, action
 ) => {
   switch (action.type) {
-    case "bookmarks/CREATE_BOOKMARK":
+    case "bookmarks/CREATE_GROUP": {
+      const index = action.payload.index === -1
+        ? state.length
+        : action.payload.index
+      const newState = [
+        ...state,
+      ]
+      newState.splice(index, 0, action.payload.group)
+      return newState
+    }
+    case 'bookmarks/DELETE_GROUP': {
+      return state.filter((_, index) => index !== action.payload)
+    }
+    case "bookmarks/EDIT_GROUP": {
+      return state.map((group, index) => {
+        if(index === action.payload.index) {
+          // TODO replace this mess below with Ramda
+          return {
+            items: group.items,
+            name: {
+              ...group.name,
+              ...action.payload.group.name
+            },
+            openAll: {
+              ...group.openAll,
+              ...action.payload.group.openAll
+            }
+          }
+        }
+        return group
+      })
+    }
+    // TODO handle moving group outside array size
+    case "bookmarks/MOVE_GROUP": {
+      const newState = [
+        ...state,
+      ]
+      const [group] = newState.splice(action.payload.from, 1)
+      newState.splice(action.payload.to, 0, group)
+      return newState
+    }
     default: return state
   }
 }
 
+// Not sure if this is the most logical location for this
+const defaultNewGroup: BookmarkGroup = {
+  name: {
+    show: true,
+    text: "",
+  },
+  openAll: {
+    show: true
+  },
+  items: [],
+}
 
 // Action Creators
 export const bookmarksActions = {
   init (): init {
     return { type: "bookmarks/INIT" }
   },
+
+  // Groups
+  createGroup (group: Omit<BookmarkGroup, 'items'> = defaultNewGroup, index: number = -1): createGroup {
+    return {
+      type: "bookmarks/CREATE_GROUP",
+      payload: {
+        group: { ...group, items: [] },
+        index,
+      }
+    }
+  },
+  deleteGroup (index: number): deleteGroup {
+    if(index === undefined) {
+      throw new Error(`deleteGroup has missing args`)
+    }
+    return {
+      type: "bookmarks/DELETE_GROUP",
+      payload: index
+    }
+  },
+  editGroup (group: Partial<BookmarkGroup>, index: number): editGroup {
+    return {
+      type: "bookmarks/EDIT_GROUP",
+      payload: {
+        group,
+        index
+      }
+    }
+  },
+  moveGroup (from: number, to: number): moveGroup {
+    return {
+      type: "bookmarks/MOVE_GROUP",
+      payload: {
+        from,
+        to,
+      }
+    }
+  },
+
+  // Bookmarks
   createBookmark (): createBookmark {
     return { type: "bookmarks/CREATE_BOOKMARK", payload: { name: 'test' } }
   },
